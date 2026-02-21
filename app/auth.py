d358c00061d402dfc03e31c7ecb6122ee85a4892
prathamkun <216220075+prathamkun@users.noreply.github.com>
2026-02-21 20:54:07 +0530

    update

--- app/auth.py
+++ app/auth.py
@@ -1,3 +1,29 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, session, url_for
 
 auth = Blueprint("auth", __name__)
+
+# temporary user database (later we use real database)
+users = {
+    "admin": "123"
+}
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username in users and users[username] == password:
+            session["user"] = username
+            return redirect(url_for("main.dashboard"))
+
+    return render_template("login.html")
+
+
+@auth.route("/logout")
+def logout():
+
+    session.pop("user", None)
+    return redirect(url_for("auth.login"))
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
-    return render_template("login.html")
+    return redirect(url_for("auth.login"))
+
+
+@main.route("/dashboard")
+def dashboard():
+
+    # protect page (must login)
+    if "user" not in session:
+        return redirect(url_for("auth.login"))
+
+    return render_template("dashboard.html")
