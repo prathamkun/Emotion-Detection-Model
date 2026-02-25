44f1a446530b9fe0874bb61d0e605ea790cc5353
prathamkun <216220075+prathamkun@users.noreply.github.com>
2026-02-25 23:37:57 +0530

    update routes.py

--- app/routes.py
+++ app/routes.py
@@ -6,11 +6,9 @@ main = Blueprint("main", __name__)
 def home():
     return redirect(url_for("auth.login"))
 
-
 @main.route("/dashboard")
 def dashboard():
 
-    # protect page (must login)
     if "user" not in session:
         return redirect(url_for("auth.login"))
 
