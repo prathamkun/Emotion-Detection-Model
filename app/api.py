577e71087c00f968dff30e77d079ad807ac9aba4
prathamkun <216220075+prathamkun@users.noreply.github.com>
2026-02-26 20:53:18 +0530

    api

--- app/api.py
+++ app/api.py
@@ -57,7 +57,7 @@ def predict():
         })
 
     # 🔥 IMPORTANT — This must be inside function
-    if "user" in session:
+    if True:
         if len(results) > 0:
             return render_template(
                 "result.html",
